c367d6e9432b7a9299a93abd2081deb97209a1f8
elizabeth-hensley_uhg <elizabeth-hensley_uhg@users.noreply.github.com>
2023-01-13 21:11:10 +0000

    deploy: uhc-tech/immerse@7caf21c0ea4a0290496d798fa638a1bcdef1993c

--- _next/static/c6VxFaw7oVAh5NxB7JJlE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:path*",destination:"/"}],fallback:[]},"/":["static/chunks/895-c97f6562eeb9d2cd.js","static/chunks/pages/index-22b773386bbbaaf5.js"],"/_error":["static/chunks/pages/_error-a1d5bc3e43249d4c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
